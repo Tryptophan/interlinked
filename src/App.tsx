b9b850f1bfa3939a800ea85278d9cac3ef4da4e5
Jacob Greenway <jagreenway12@gmail.com>
2024-04-13 16:12:08 -0700

    cultural context

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useCallback, useState } from "react";
 import "./App.css";
 import { LiveTranscriptionEvents, createClient } from "@deepgram/sdk";
 import OpenAI from "openai";
+import { FaMicrophone } from "react-icons/fa";
 
 const deepgram = createClient(import.meta.env.VITE_DEEPGRAM_API_KEY!);
 const openai = new OpenAI({
@@ -22,13 +23,44 @@ function App() {
     []
   );
 
+  const translate = useCallback(
+    async (from: string, to: string, text: string) => {
+      try {
+        const response = await fetch(
+          "https://interlinked.auto.movie/api/translate",
+          {
+            method: "POST",
+            headers: {
+              Accept: "application/json",
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              from_lang: from,
+              to_lang: to,
+              text: text,
+            }),
+          }
+        );
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch data");
+        }
+
+        return (await response.json()).translated_text;
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    },
+    []
+  );
+
   const generateCulturalContext = useCallback(
     async (text: string, language: string) => {
       const contextResponse = await openai.chat.completions.create({
         messages: [
           {
             role: "system",
-            content: `You will be provided with text in language ${language}, your task is to return an explanation of a famous person/place/thing (if none found, reply only with ##NO_REPLY##) from the text in the language ${language}. Keep your response to 2 sentences.`,
+            content: `You will be provided with text in language ${language}, your task is to return an explanation of a famous person/place/thing (if none found, reply only with ##NO_REPLY##) from the text in the language ${language}. Keep your response to 1 short sentence.`,
           },
           { role: "user", content: text },
         ],
@@ -75,23 +107,15 @@ function App() {
             if (text && text.length) {
               console.log(text);
               setSourceTranscript((prevState) => [...prevState, text]);
-              // Translate
-              const translateResponse = await openai.chat.completions.create({
-                messages: [
-                  {
-                    role: "system",
-                    content: `You will be provided with a sentence in ${sourceLanguage}, and your task is to translate it into ${targetLanguage}.`,
-                  },
-                  { role: "user", content: text },
-                ],
-                model: "gpt-3.5-turbo",
-              });
-
-              const translation = translateResponse.choices[0].message.content;
+              const translation = await translate(
+                sourceLanguage,
+                targetLanguage,
+                text
+              );
               if (translation) {
                 setTargetTranscript((prevState) => [...prevState, translation]);
                 // Generate cultural context
-                // await generateCulturalContext(translation, targetLanguage);
+                await generateCulturalContext(translation, targetLanguage);
               }
             }
           }
@@ -112,33 +136,62 @@ function App() {
   return (
     <>
       <div className="App">
-        <div className="Left">
-          Source Language:
-          <select
-            value={sourceLanguage}
-            onChange={(event) => setSourceLanguage(event.target.value)}
-          >
-            <option value="en-US">English</option>
-            <option value="zh">Chinese</option>
-          </select>
-          Target Language:
-          <select
-            value={targetLanguage}
-            onChange={(event) => setTargetLanguage(event.target.value)}
-          >
-            <option value="en-US">English</option>
-            <option value="zh">Chinese</option>
-          </select>
-          <button onClick={listen}>{!listening ? "Start" : "Stop"}</button>
-          Source Transcript:
-          <div>{sourceTranscript.join(" ")}</div>
-          Target Transcript:
-          <div>{targetTranscript.join(" ")}</div>
-        </div>
-        <div className="Right">
-          {contexts.map((context, index) => (
-            <div key={index}>{context.text}</div>
-          ))}
+        <h1>Try Interlinked AI</h1>
+        <h2>
+          AI-powered live and real-time speech translation across 100+ languages
+          with cultural context explanation, perfect for meetings, calls,
+          videos, webinars, and conferences.
+        </h2>
+        <div className="Translator">
+          <div className="Left">
+            <div>Step 1: Select source and target languages</div>
+            <div className="LanguageSelect">
+              Translate From
+              <select
+                value={sourceLanguage}
+                onChange={(event) => setSourceLanguage(event.target.value)}
+              >
+                <option value="en-US">English</option>
+                <option value="zh">Chinese</option>
+              </select>
+            </div>
+            <div className="LanguageSelect">
+              Translate To
+              <select
+                value={targetLanguage}
+                onChange={(event) => setTargetLanguage(event.target.value)}
+              >
+                <option value="en-US">English</option>
+                <option value="zh">Chinese</option>
+              </select>
+            </div>
+            <div>
+              Step 2: Enable the Microphone and speak in your own language
+            </div>
+            <FaMicrophone className="MicToggle" onClick={listen} />
+          </div>
+          <div className="Right">
+            <div>
+              Step 3: View the live transcription and translation in 100+
+              different languages
+            </div>
+            <div className="Transcripts">
+              {!sourceTranscript.length || !targetTranscript.length ? (
+                "Translator is starting..."
+              ) : (
+                <>
+                  <div>{sourceTranscript.join(" ")}</div>
+                  <div>{targetTranscript.join(" ")}</div>
+                </>
+              )}
+            </div>
+          </div>
+          <div>
+            <div>Step 4: See the cultural explanation for understanding</div>
+            {contexts.map((context, index) => (
+              <div key={index}>{context.text}</div>
+            ))}
+          </div>
         </div>
       </div>
     </>
